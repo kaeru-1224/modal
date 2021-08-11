6af4370ab1986756dc4a4f3f705f878bb2d6dfc6
JISU <cjfslzpf@naver.com>
2021-08-11 13:25:06 +0900

    0811

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//zindex로 처음에는  body를 바꿔주려했는데 안되어서 ㅋㅋㅋㅋ
+//결국 방법을 찾아보니 그냥  div를 크게 만들어서 덮어씌워주고 
+//div의 zindex를 조절해주면 되는 거였어 흑흑,,,,, 
+
+
+
 const btn = document.querySelector("button")
 const over = document.querySelector(".overlay")
 const closeBtn= document.querySelector(".box-close")
